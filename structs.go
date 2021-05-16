005f3c3051b696657d50763f4e3a6c0ee9570cfa
Brayan Lopez <brxyxn.corp@live.com>
2021-05-15 21:53:02 -0600

    Added structs.go

---
+++ structs.go
@@ -0,0 +1,9 @@
+package main
+
+// Restaurant structures
+type Restaurants struct {
+	Id          int    `json:"id"`
+	Name        string `json:"name"`
+	Description string `json:"description"`
+	Image_url   string `json:"image_url"`
+}
